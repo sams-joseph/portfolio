61ed3e4de2ebd6273bef1e7a8cd42821e0a3d59a
Joseph Sams <sams_josep@yahoo.com>
2018-07-16 09:26:54 -0400

    Removed unused plugins for gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,22 +5,5 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'img',
-        path: `${__dirname}/src/images`
-      }
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'src',
-        path: `${__dirname}/src/`
-      }
-    },
-    'gatsby-transformer-remark',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp'
   ],
 }
