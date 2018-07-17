1a1185c5e1a62c073ceacc1fe0fc1c0e71ea7fc8
Joseph Sams <joe.mac@Josephs-iMac.local>
2018-07-16 23:41:14 -0400

    Fixed bug with gatsby build config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,16 +2,5 @@ module.exports = {
   siteMetadata: {
     title: 'Joseph Sams',
   },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-styled-components',
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/posts`,
-        name: 'markdown-pages',
-      },
-    },
-    'gatsby-transformer-remark'
-  ],
-}
+  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-styled-components'],
+};
